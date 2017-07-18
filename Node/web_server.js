9804257adbe46a1f7c4ae82dd041bb5eeafeb192
Ken Wagner <ken_xwagner@hotmail.com>
2017-07-18 02:23:05 -0700

    Debugged
    Fixed syntax error

--- Node/web_server.js
+++ Node/web_server.js
@@ -1,7 +1,7 @@
-var http: =  require("http");
+var http =  require("http");
 var server = http.createServer(function(request, response) {
 	response.writeHead(200, {"Content-Type": "text/html"});
-	response.write("<!DOCTYPE "html">");
+	response.write("<!DOCTYPE html>");
 	response.write("<html>");
 	response.write("<head>");
 	response.write("<title>Welcome to the Page</title>");
